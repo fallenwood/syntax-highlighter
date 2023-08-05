b9d1df3719558f3c70239e7dfee0c33beb80ca8a
Fallenwood <fallenwood.y@outlook.com>
2023-08-05 09:07:36 +0800

    tmp

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const os = require("os");
 
 // Languages
 let langs = [];
@@ -12,39 +13,81 @@ fs.readdirSync(__dirname + "/../grammars/").forEach(name => {
 
 // Language-package map
 const langMap = {
-    typescript: ["typescript", "typescript"],
-    typescriptreact: ["typescript", "tsx"],
-    ocaml: ["ocaml", "ocaml"],
-    shellscript: ["bash"]
+    typescript: {
+        module: ["typescript", "typescript"],
+        output: "typescript",
+    },
+    typescriptreact: {
+        module: ["typescript", "tsx"],
+        output: "tsx",
+    },
+    ocaml:{
+        module: ["ocaml", "ocaml"],
+        output: "ocaml",
+    },
+    shellscript:{
+        module: ["bash"],
+        output: "bash",
+    },
+    csharp: {
+        module: ["c-sharp"],
+        output: "c_sharp",
+    },
 }
 
 // Build wasm parsers for supported languages
-const parsersDir = __dirname + "/../parsers";
+const parsersDir = path.resolve(path.join(__dirname, "..", "parsers"));
 if (!fs.existsSync(parsersDir)) {
     fs.mkdirSync(parsersDir);
 }
 for (li of langs) {
     const lang = li;
-    let module = "node_modules/tree-sitter-" + lang;
+    let module = path.resolve(path.join(__dirname, "..", "node_modules", `tree-sitter-${lang}`));
     let output = "tree-sitter-" + lang + ".wasm";
+
     if (langMap[lang]) {
-        module = path.join("node_modules/tree-sitter-" +
-            langMap[lang][0], ...langMap[lang].slice(1))
-        output = "tree-sitter-" + langMap[lang][langMap[lang].length - 1] + ".wasm";
+        module = path.join(
+            __dirname,
+            "..",
+            "node_modules",
+            "tree-sitter-" + langMap[lang].module[0],
+            ...langMap[lang].module.slice(1),
+        );
+
+        output = "tree-sitter-" + langMap[lang].output + ".wasm";
     }
 
-    console.log("Compiling " + lang + " parser");
-    exec("node_modules/.bin/tree-sitter build-wasm " + module,
+    console.log(`Compiling ${lang} parser with ${module} to ${output}`);
+
+    if (!fs.existsSync(module)) {
+        console.error("No module found for " + lang);
+        continue;
+    }
+
+    let executable = path.join(__dirname, "..", "node_modules", ".bin", "tree-sitter");
+
+    if (os.platform() === "win32") {
+        executable += ".cmd";
+    }
+
+    executable = path.resolve(executable);
+
+    exec(`${executable} build-wasm ${module}`,
         (err) => {
-            if (err)
-                console.log("Failed to build wasm for " + lang + ": " + err.message);
-            else
-                fs.rename(output, "parsers/" + lang + ".wasm",
+            if (err) {
+                console.error("Failed to build wasm for " + lang + ": " + err.message);
+            } else {
+                fs.rename(
+                    output,
+                    "parsers/" + lang + ".wasm",
                     (err) => {
-                        if (err)
-                            console.log("Failed to copy built parser: " + err.message);
-                        else
+                        if (err) {
+                            console.error("Failed to copy built parser: " + err.message);
+                        }
+                        else {
                             console.log("Successfully compiled " + lang + " parser");
+                        }
                     });
+            }
         });
 }
